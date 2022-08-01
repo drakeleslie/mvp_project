3f465adc35a0e2096028797ca183cd6fec17e422
drakeleslie <drake.j.leslie@gmail.com>
2022-08-01 15:40:18 -0500

    css

--- static/app.js
+++ static/app.js
@@ -4,17 +4,5 @@ const body = document.body;
 
 $.get("/ppl", (data) => {
   let result = JSON.stringify(data);
-  document.write(result);
-});
-$.get("/ppl/exercises", (data) => {
-  console.log(data);
-});
-$.get("/ppl/exercises/push", (data) => {
-  console.log(data);
-});
-$.get("/ppl/exercises/pull", (data) => {
-  console.log(data);
-});
-$.get("/ppl/exercises/legs", (data) => {
-  console.log(data);
+  //document.write(result);
 });
